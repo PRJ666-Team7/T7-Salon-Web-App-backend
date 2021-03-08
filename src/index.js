b259e83a6eeda65d59563dac6613b18135ce742e
unknown <jaspermui@hotmail.ca>
2021-03-08 11:37:20 -0500

    add headers

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ const app = express();
 const port = 8000;
 var routes = require('./routes/login')(app);
 
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+  next();
+});
+
 app.get('/', function(req, res) {
-  res.write("Hi I am the root")
-  res.end();
+  res.send('Hi i am root')
 })
 
 app.listen(port, () => {
