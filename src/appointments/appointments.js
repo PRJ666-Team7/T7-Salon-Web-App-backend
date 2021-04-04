02d905c8e75b851512e8e3a0c573844adae72fcc
unknown <jaspermui@hotmail.ca>
2021-04-04 02:24:19 -0400

    scheduling

--- src/appointments/appointments.js
+++ src/appointments/appointments.js
@@ -137,3 +137,32 @@ module.exports.addAppointments = async (data, usrId) => {
     return null;
   }
 };
+
+module.exports.adminAddAppointment = async (emp, date, startTime, endTime) => {
+  try {
+    console.log("emp, date, startTime, endTime", emp, date, startTime, endTime)
+      const query = {
+          text: 'INSERT INTO appointment( emp_id, apt_date, apt_time_start, apt_time_end) VALUES($1, $2, $3, $4) RETURNING *',
+          values: [emp, date, startTime, endTime],
+      }
+      const res = await client.query(query)
+
+      return res.rows[0]
+  } catch (err) {
+      console.log(err.stack)
+  }
+}
+
+module.exports.getAppointmentsByEmployee = async (id, date) => {
+  try {
+      const query = {
+          text: 'SELECT * from appointment where emp_id = $1 and apt_date= $2',
+          values: [id, date]
+      }
+      const res = await client.query(query)
+
+      return res.rows
+  } catch (err) {
+      console.log(err.stack)
+  }
+}
---
+++ src/routes/admin.js
@@ -0,0 +1,72 @@
+
+const passport = require("passport");
+const { body, validationResult } = require("express-validator");
+const adminDataService = require('../dataService/admin/admin')
+const appointmentDataService = require('../appointments/appointments')
+var moment = require('moment'); 
+
+module.exports = function async (app) {
+    app.post(
+        "/scheduleEmployee",
+        body("employee").isLength({ min: 1 }),
+        body("date").isLength({ min: 1 }),
+        body("startTime").isLength({ min: 1 }),
+        body("endTime").isLength({ min: 1 }),
+        passport.authenticate('jwt', { session: false }),
+        async (req, res) => {
+            console.log("req.body", req.body)
+            //   if(!req.user.isAdmin){
+            //     return res.json({
+            //       status: "fail"
+            //     })
+            //   }
+            try {
+                const errors = validationResult(req);
+
+                const errorDates = []
+                if (!errors.isEmpty()) {
+                    return res.json({
+                        status: "fail",
+                        err: errors.msg + ": " + errors.param,
+                    });
+                } else {
+                    const dayStartTime = moment(req.body.startTime, 'hh:mm');
+                    const dayEndTime = moment(req.body.endTime, 'hh:mm');
+                    const numApt = dayEndTime.diff(dayStartTime, "minute")/30;
+
+                    await Promise.all(req.body.date.map(async (d) =>{
+                        const result = await appointmentDataService.getAppointmentsByEmployee(req.body.employee, d)
+
+                        console.log("result", result)
+                        const startTime = moment(req.body.startTime, 'hh:mm');
+                        const endTime = moment(req.body.startTime, 'hh:mm');
+                        endTime.add(30, "minutes")
+
+                        const aptExist = result.find(a => a.apt_time_start == startTime.format("hh:mm:ss"))
+                        if (aptExist == undefined){
+                            console.log("startTime", startTime)
+                            const addAptResult = await appointmentDataService.adminAddAppointment(req.body.employee, d, startTime.format("hh:mm"), endTime.format("hh:mm"))
+                        }
+
+                        for (let i = 1; i < numApt; i++) {
+                            startTime.add(30, "minutes").format("hh:mm") 
+                            endTime.add(30, "minutes").format("hh:mm")
+
+                            const aptExist = result.find(a => a.apt_time_start == startTime.format("hh:mm:ss"))
+                            if (aptExist == undefined){
+                                console.log("startTime", startTime)
+                                const addAptResult = await appointmentDataService.adminAddAppointment(req.body.employee, d, startTime.format("hh:mm"), endTime.format("hh:mm"))
+                            }
+                        }
+                    }))
+
+                    res.send({status: "success"});
+                }
+            } catch (error) {
+                console.log("post /scheduleEmployee error", error)
+                return res.json({ status: "fail" });
+            }
+        }
+    );
+
+};
