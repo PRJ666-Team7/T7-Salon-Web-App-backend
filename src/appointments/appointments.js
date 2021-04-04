2b9127832770d8bf5211e7ba05cad379db49cbf5
unknown <jaspermui@hotmail.ca>
2021-04-04 17:43:42 -0400

    get appointment fix

--- src/appointments/appointments.js
+++ src/appointments/appointments.js
@@ -12,7 +12,7 @@ client.connect();
 
 module.exports.getAppointments = async (id) => {
   try {
-    sqlLine = `SELECT APPOINTMENT.APT_ID AS "id", CONCAT(USERS.USR_FNAME, ' ', USERS.USR_LNAME) AS "name", USERS.USR_PHONE AS "phone", CONCAT(APPOINTMENT.APT_DATE, ' ', APPOINTMENT.APT_TIME) AS "time" FROM APPOINTMENT INNER JOIN USERS ON APPOINTMENT.USR_ID = USERS.USR_ID WHERE APPOINTMENT.emp_id = (select emp_id from employee where usr_id = ` + id + `) ORDER BY APPOINTMENT.APT_DATE, APT_TIME`;
+    sqlLine = `SELECT APPOINTMENT.APT_ID AS "id", CONCAT(USERS.USR_FNAME, ' ', USERS.USR_LNAME) AS "name", USERS.USR_PHONE AS "phone", CONCAT(APPOINTMENT.APT_DATE, ' ', APPOINTMENT.APT_TIME_start) AS "time" FROM APPOINTMENT INNER JOIN USERS ON APPOINTMENT.USR_ID = USERS.USR_ID WHERE APPOINTMENT.emp_id = (select emp_id from employee where usr_id = ` + id + `) ORDER BY APPOINTMENT.APT_DATE, APT_TIME_START`;
 
     const query = {
       text: sqlLine,
