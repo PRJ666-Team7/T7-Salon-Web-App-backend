a9f92dfbd97ae914ca6de29058d4d6b4fa5a35eb
zjjiang2 <zjjiang2@myseneca.ca>
2021-04-05 04:34:06 -0400

    MS4

--- src/employees/employees.js
+++ src/employees/employees.js
@@ -41,6 +41,19 @@ module.exports.getEmployeeName = async (id) => {
   }
 };
 
+module.exports.getEmployeeByUserID = async (userID) => {
+  try{
+    sqlLine = `SELECT EMP_ID FROM EMPLOYEE WHERE USR_ID =` + userID
+    const query = {
+      text: sqlLine,
+    }
+    const res = await client.query(query);
+
+    return res.rows[0];
+  } catch (err) {
+    console.log(err.stack);
+  }
+};
 
 module.exports.setEmployee = async (id, emp) => {
   try {
@@ -48,8 +61,56 @@ module.exports.setEmployee = async (id, emp) => {
       sqlLine = `INSERT INTO EMPLOYEE (USR_ID) VALUES (` + id + `)`
     }
     else{
-      sqlLine = `DELETE FROM EMPLOYEE WHERE USR_ID = ` + id + `;`
+      sqlLine = `DELETE FROM EMPLOYEE WHERE USR_ID = ` + id
     }
+
+    const query = {
+      text: sqlLine,
+    };
+
+    const res = await client.query(query);
+
+    return res.rows;
+  } catch (err) {
+    console.log(err.stack);
+  }
+};
+
+module.exports.getEmployeeAppointments = async (empId) => {
+  try {
+    sqlLine = `SELECT * FROM APPOINTMENT WHERE EMP_ID = ` + empId;
+
+    const query = {
+      text: sqlLine,
+    };
+
+    const res = await client.query(query);
+
+    return res.rows;
+  } catch (err) {
+    console.log(err.stack);
+  }
+};
+
+module.exports.getEmployeeAvailableTime = async (userId) => {
+  try {
+    sqlLine = `SELECT APPOINTMENT.APT_ID AS "id", APPOINTMENT.APT_DATE AS "date", APPOINTMENT.APT_TIME_START AS "time_start", APPOINTMENT.APT_TIME_END AS "time_end" FROM APPOINTMENT WHERE APPOINTMENT.emp_id = (select emp_id from employee where usr_id = ` + userId + `) AND APT_DATE >= NOW() AND AVAILABLE = TRUE ORDER BY APPOINTMENT.APT_DATE, APT_TIME_START`;
+
+    const query = {
+      text: sqlLine,
+    };
+
+    const res = await client.query(query);
+
+    return res.rows;
+  } catch (err) {
+    console.log(err.stack);
+  }
+};
+
+module.exports.getUserInfo = async (term) => {
+  try {
+    sqlLine = `SELECT USR_ID, CONCAT(USR_LNAME, ' ', USR_FNAME) AS "name", USR_EMAIL AS "email", ISADMIN AS "admin", (SELECT COUNT(E.EMP_ID) FROM EMPLOYEE E WHERE U.USR_ID = E.USR_ID) AS "employee" FROM USERS U WHERE USR_EMAIL LIKE '%` + term + `%' ORDER BY USR_EMAIL`
 
     const query = {
       text: sqlLine,
@@ -65,7 +126,7 @@ module.exports.setEmployee = async (id, emp) => {
 
 module.exports.setAdmin = async (id) => {
   try {
-    sqlLine = `uPDATE USERS SET ISADMIN = NOT ISADMIN WHERE USR_ID = ` + id;
+    sqlLine = `UPDATE USERS SET ISADMIN = NOT ISADMIN WHERE USR_ID = ` + id;
 
     const query = {
       text: sqlLine,
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,73 +1,192 @@
-
 const passport = require("passport");
 const { body, validationResult } = require("express-validator");
-const adminDataService = require('../dataService/admin/admin')
-const appointmentDataService = require('../appointments/appointments')
-var moment = require('moment'); 
+const adminDataService = require("../dataService/admin/admin");
+const appointmentDataService = require("../appointments/appointments");
+const employees = require("../employees/employees");
+var moment = require("moment");
 
-module.exports = function async (app) {
-    app.post(
-        "/scheduleEmployee",
-        body("employee").isLength({ min: 1 }),
-        body("date").isLength({ min: 1 }),
-        body("startTime").isLength({ min: 1 }),
-        body("endTime").isLength({ min: 1 }),
-        passport.authenticate('jwt', { session: false }),
-        async (req, res) => {
-            console.log("req.body", req.body)
-            //   if(!req.user.isAdmin){
-            //     return res.json({
-            //       status: "fail"
-            //     })
-            //   }
-            try {
-                const errors = validationResult(req);
+module.exports = function async(app) {
+  app.post(
+    "/scheduleEmployee",
+    body("employee").isLength({ min: 1 }),
+    body("date").isLength({ min: 1 }),
+    body("startTime").isLength({ min: 1 }),
+    body("endTime").isLength({ min: 1 }),
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+      console.log("req.body", req.body);
+      //   if(!req.user.isAdmin){
+      //     return res.json({
+      //       status: "fail"
+      //     })
+      //   }
+      try {
+        const errors = validationResult(req);
 
-                const errorDates = []
-                if (!errors.isEmpty()) {
-                    return res.json({
-                        status: "fail",
-                        err: errors.msg + ": " + errors.param,
-                    });
-                } else {
-                    const dayStartTime = moment(req.body.startTime, 'hh:mm');
-                    const dayEndTime = moment(req.body.endTime, 'hh:mm');
-                    const numApt = dayEndTime.diff(dayStartTime, "minute")/30;
+        const errorDates = [];
+        if (!errors.isEmpty()) {
+          return res.json({
+            status: "fail",
+            err: errors.msg + ": " + errors.param,
+          });
+        } else {
+          const dayStartTime = moment(req.body.startTime, "hh:mm");
+          const dayEndTime = moment(req.body.endTime, "hh:mm");
+          const numApt = dayEndTime.diff(dayStartTime, "minute") / 30;
 
-                    await Promise.all(req.body.date.map(async (d) =>{
-                        const result = await appointmentDataService.getAppointmentsByEmployee(req.body.employee, d)
+          await Promise.all(
+            req.body.date.map(async (d) => {
+              const result = await appointmentDataService.getAppointmentsByEmployee(
+                req.body.employee,
+                d
+              );
 
-                        console.log("result", result)
-                        const startTime = moment(req.body.startTime, 'hh:mm');
-                        const endTime = moment(req.body.startTime, 'hh:mm');
-                        endTime.add(30, "minutes")
+              console.log("result", result);
+              const startTime = moment(req.body.startTime, "hh:mm");
+              const endTime = moment(req.body.startTime, "hh:mm");
+              endTime.add(30, "minutes");
 
-                        const aptExist = result.find(a => a.apt_time_start == startTime.format("hh:mm:ss"))
-                        if (aptExist == undefined){
-                            console.log("startTime", startTime)
-                            const addAptResult = await appointmentDataService.adminAddAppointment(req.body.employee, d, startTime.format("hh:mm"), endTime.format("hh:mm"))
-                        }
+              const aptExist = result.find(
+                (a) => a.apt_time_start == startTime.format("hh:mm:ss")
+              );
+              if (aptExist == undefined) {
+                console.log("startTime", startTime);
+                const addAptResult = await appointmentDataService.adminAddAppointment(
+                  req.body.employee,
+                  d,
+                  startTime.format("hh:mm"),
+                  endTime.format("hh:mm")
+                );
+              }
 
-                        for (let i = 1; i < numApt; i++) {
-                            startTime.add(30, "minutes").format("hh:mm") 
-                            endTime.add(30, "minutes").format("hh:mm")
+              for (let i = 1; i < numApt; i++) {
+                startTime.add(30, "minutes").format("hh:mm");
+                endTime.add(30, "minutes").format("hh:mm");
 
-                            const aptExist = result.find(a => a.apt_time_start == startTime.format("hh:mm:ss"))
-                            if (aptExist == undefined){
-                                console.log("startTime", startTime)
-                                const addAptResult = await appointmentDataService.adminAddAppointment(req.body.employee, d, startTime.format("hh:mm"), endTime.format("hh:mm"))
-                            }
-                        }
-                    }))
-
-                    res.send({status: "success"});
+                const aptExist = result.find(
+                  (a) => a.apt_time_start == startTime.format("hh:mm:ss")
+                );
+                if (aptExist == undefined) {
+                  console.log("startTime", startTime);
+                  const addAptResult = await appointmentDataService.adminAddAppointment(
+                    req.body.employee,
+                    d,
+                    startTime.format("hh:mm"),
+                    endTime.format("hh:mm")
+                  );
                 }
-            } catch (error) {
-                console.log("post /scheduleEmployee error", error)
-                return res.json({ status: "fail" });
-            }
-        }
-    );
+              }
+            })
+          );
 
->>>>>>> 02d905c8e75b851512e8e3a0c573844adae72fcc
+          res.send({ status: "success" });
+        }
+      } catch (error) {
+        console.log("post /scheduleEmployee error", error);
+        return res.json({ status: "fail" });
+      }
+    }
+  );
+
+  app.get(
+    "/getUsrInfo",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+      if(!req.user.isAdmin){
+        return res.json({
+          status: "fail"
+        })
+      }
+      const userData = await employees.getUserInfo(req.query.term);
+      //const userData = await employees.getUserInfo("jack");
+      res.send(userData);
+    }
+  );
+
+  app.post(
+    "/setEmployee",
+    body("id").isLength({ min: 1, max: 5 }),
+    body("employee").isBoolean(),
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+      if (!req.user.isAdmin) {
+        return res.json({
+          status: "fail",
+        });
+      }
+      try {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+          return res.json({
+            status: "fail",
+            err: errors.msg + ": " + errors.param,
+          });
+        } else {
+          var employee;
+          if (req.body.employee === true) {
+            employee = await employees.setEmployee(
+              req.body.id,
+              req.body.employee
+            );
+
+            res.send(employee);
+          } else if (req.body.employee === false) {
+            employee = await employees.setEmployee(
+              req.body.id,
+              req.body.employee
+            );
+
+            res.send(employee);
+          }
+        }
+      } catch (error) {
+        return res.json({ status: "fail" });
+      }
+    }
+  );
+
+  app.post(
+    "/setAdmin",
+    body("id").isLength({ min: 1, max: 5 }),
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+      if (!req.user.isAdmin) {
+        return res.json({
+          status: "fail",
+        });
+      }
+      try {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+          return res.json({
+            status: "fail",
+            err: errors.msg + ": " + errors.param,
+          });
+        } else {
+          const admin = await employees.setAdmin(req.body.id);
+
+          res.send(admin);
+        }
+      } catch (error) {
+        return res.json({ status: "fail" });
+      }
+    }
+  );
+
+  app.get("/getEmpId", passport.authenticate("jwt", { session: false }), async (req, res) => {
+    // if (!req.user.isAdmin) {
+    //   console.log("not admin!")
+    // }
+
+    const empId = await employees.getEmployeeByUserID(req.query.id);
+    res.send(empId);
+  });
+
+  
+  app.get("/getEmpApt", passport.authenticate("jwt", { session: false }), async (req, res) => {
+    const empApt = await employees.getEmployeeAppointments(req.query.id);
+    res.send(empApt);
+  });
 };
