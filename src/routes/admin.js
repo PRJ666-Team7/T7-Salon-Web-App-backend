636f1922a33ab3f3c703aaf51cc19b714c13ba3d
unknown <jaspermui@hotmail.ca>
2021-04-05 06:39:13 -0400

    fix schedule

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -36,22 +36,22 @@ module.exports = function async(app) {
                     await Promise.all(req.body.date.map(async (d) =>{
                         const result = await appointmentDataService.getAppointmentsByEmployee(req.body.employee, d)
 
-                        const startTime = moment(req.body.startTime, 'hh:mm');
-                        const endTime = moment(req.body.startTime, 'hh:mm');
+                        const startTime = moment(req.body.startTime, 'HH:mm');
+                        const endTime = moment(req.body.startTime, 'HH:mm');
                         endTime.add(30, "minutes")
 
                         const aptExist = result.find(a => a.apt_time_start == startTime.format("hh:mm:ss"))
                         if (aptExist == undefined){
-                            const addAptResult = await appointmentDataService.adminAddAppointment(req.body.employee, d, startTime.format("hh:mm"), endTime.format("hh:mm"))
+                            const addAptResult = await appointmentDataService.adminAddAppointment(req.body.employee, d, startTime.format("HH:mm"), endTime.format("HH:mm"))
                         }
 
                         for (let i = 1; i < numApt; i++) {
-                            startTime.add(30, "minutes").format("hh:mm") 
-                            endTime.add(30, "minutes").format("hh:mm")
+                            startTime.add(30, "minutes")
+                            endTime.add(30, "minutes")
 
-                            const aptExist = result.find(a => a.apt_time_start == startTime.format("hh:mm:ss"))
+                            const aptExist = result.find(a => a.apt_time_start == startTime.format("HH:MM:ss"))
                             if (aptExist == undefined){
-                                const addAptResult = await appointmentDataService.adminAddAppointment(req.body.employee, d, startTime.format("hh:mm"), endTime.format("hh:mm"))
+                                const addAptResult = await appointmentDataService.adminAddAppointment(req.body.employee, d, startTime.format("HH:mm"), endTime.format("HH:mm"))
                             }
                         }
                     }))
